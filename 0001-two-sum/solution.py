5fa0dd7ade75ad3f03eedf9419bfa24ba48e3740
Abel Mekonnen <abelmekonen247@gmail.com>
2025-12-09 20:56:10 +0300

    Update solution for 0001-two-sum/solution.py

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,1 +1,11 @@
-c
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        leng = len(nums)
+        hash_map = {}
+        for i in range(len(nums)):
+            num2 = target - nums[i]
+            if num2 in hash_map:
+                return [hash_map[num2], i]
+            else:
+                hash_map[nums[i]] = i 
+        return [-1,-1]
