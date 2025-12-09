0f85fc45c4920ddd0fdb06e788275b1c24759e01
Abel Mekonnen <abelmekonen247@gmail.com>
2025-12-10 01:11:34 +0300

    Update solution for 3885-count-special-triplets/solution.py

--- 3885-count-special-triplets/solution.py
+++ 3885-count-special-triplets/solution.py
@@ -1,19 +1,20 @@
+
+class Solution:
+    def specialTriplets(self, nums: List[int]) -> int:
+        mod = 10**9 + 7
+        total_count = 0
+
+        counter = Counter()
+        left_needs_counter = Counter()
+
+        for i, num in enumerate(nums):
+            need = num * 2
+            left_needs_counter[i] = counter[need]
             counter[num] += 1
 
         for index, left_needs in left_needs_counter.items():
             need = nums[index] * 2
             total_needs = counter[need]
-            if nums[index] == 0:
 
             # in the case of zero we need to remove 1 from the total needs cause current zero is also considered and we don't want that 
-            left_needs_counter[i] = counter[need]
-            need = num * 2
-        for i, num in enumerate(nums):
-        left_needs_counter = Counter()
-
-
-        counter = Counter()
-        mod = 10**9 + 7
-        total_count = 0
-    def specialTriplets(self, nums: List[int]) -> int:
-class Solution:
+            if nums[index] == 0:
