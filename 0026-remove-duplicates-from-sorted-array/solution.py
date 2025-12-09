fd542d1115a5ffde9156c9e186c8e71d899f65de
Abel Mekonnen <abelmekonen247@gmail.com>
2025-12-09 18:49:08 +0300

    Update solution for 0026-remove-duplicates-from-sorted-array/solution.py

--- 0026-remove-duplicates-from-sorted-array/solution.py
+++ 0026-remove-duplicates-from-sorted-array/solution.py
@@ -1,10 +1,4 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        i = 0
-        n = len(nums)
-   
-        for j in range(n):
-            if nums[i] == nums[j]:
-                continue
-            
-            i += 1
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
