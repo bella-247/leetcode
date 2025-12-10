816bb598d2bcb7fc8f711d92b9a385ea7147b3a6
Abel Mekonnen <abelmekonen247@gmail.com>
2025-12-10 13:21:10 +0300

    Update solution for 3885-count-special-triplets/solution.py

--- 3885-count-special-triplets/solution.py
+++ 3885-count-special-triplets/solution.py
@@ -1,1 +1,29 @@
-leftCount * rightCount
+class Solution:
+    def specialTriplets(self, nums: List[int]) -> int:
+        n = len(nums)
+        mod = 10**9 + 7
+        total_count = 0
+
+        freq = Counter()
+        left_needs_count = [0] * n
+
+        for i in range(n):
+            num = nums[i]
+            need = num * 2
+            left_needs_count[i] = freq[need]
+            freq[num] += 1
+
+        for i in range(n):
+            need = nums[i] * 2
+            left_needs = left_needs_count[i]
+            total_needs = freq[need]
+
+            if nums[i] == need:
+                total_needs -= 1
+
+            right_needs = total_needs - left_needs
+
+            total_count += (left_needs * right_needs)
+
+
+        return total_count % mod
