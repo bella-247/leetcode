b1d815c7bb7fd1f36f8d4985ea52be76e5f3bb73
Abel Mekonnen <abelmekonen247@gmail.com>
2025-12-11 15:11:08 +0300

    Update solution for 0009-palindrome-number/solution.py

--- 0009-palindrome-number/solution.py
+++ 0009-palindrome-number/solution.py
@@ -1,5 +1,13 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        str_num = str(x)
-        return str_num == str_num[::-1]
+        if x < 0:
+            return False
+        x_copy = x
+        reverse = 0
+        while x != 0:
+            digit = x % 10
+            reverse = reverse * 10 + digit
+            x //= 10
+
+        return x_copy == reverse
         
