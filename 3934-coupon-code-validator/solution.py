45c018332dfdf25e3260c45d5523dd8d88df3385
Abel Mekonnen <abelmekonen247@gmail.com>
2025-12-16 22:36:48 +0300

    Add solution for 3934-coupon-code-validator/solution.py

---
+++ 3934-coupon-code-validator/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validateCoupons(self, code: List[str], businessLine: List[str], isActive: List[bool]) -> List[str]:
+        lines = {"electronics", "grocery", "pharmacy", "restaurant"}
+        def isValid(i):
+            if code[i] and isActive[i] and businessLine[i] in lines and all(c.isalnum() or c == "_" for c in code[i]):
+                return True
+
+            return False
+
+        n = len(code)
+        valids = [
+            (businessLine[i], code[i])
+            for i in range(n)
+            if isValid(i)
+        ]
+
+        valids.sort()
+
+        return [c for _, c in valids]
+
+        
