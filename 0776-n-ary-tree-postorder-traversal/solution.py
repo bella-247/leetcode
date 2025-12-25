8fd832c9fd1337c95f8040fd6a232b2bfe575628
Abel Mekonnen <abelmekonen247@gmail.com>
2025-12-25 13:20:47 +0300

    Add solution for 0776-n-ary-tree-postorder-traversal/solution.py

---
+++ 0776-n-ary-tree-postorder-traversal/solution.py
@@ -0,0 +1,19 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+
+        children_result = []
+        for child in root.children:
+            children_result += self.postorder(child)
+
+        return children_result + [root.val]
+        
